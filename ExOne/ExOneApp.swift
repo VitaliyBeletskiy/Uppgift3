bd126c010ab8002ed5c635442f9b0c83560b7a90
Vitaliy Beletskiy <vitaliy.beletskiy@gmail.com>
2023-10-01 14:36:48 +0200

    Initial Commit

---
+++ ExOne/ExOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExOneApp.swift
+//  ExOne
+//
+//  Created by Vitaliy on 2023-10-01.
+//
+
+import SwiftUI
+
+@main
+struct ExOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
